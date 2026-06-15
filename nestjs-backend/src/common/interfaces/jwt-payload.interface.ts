9e3c1a74246f33d4d376c1248a1b6c4f8641b032
Ashish Sharma <ashu11vats@gmail.com>
2026-06-15 12:32:51 +0530

    Updated jwt-payload.interface.ts

--- nestjs-backend/src/common/interfaces/jwt-payload.interface.ts
+++ nestjs-backend/src/common/interfaces/jwt-payload.interface.ts
@@ -1,9 +1,11 @@
 /* eslint-disable prettier/prettier */
 
+import { Role } from '../constants/roles.constants';
+
 export interface JwtPayload {
   sub: string;
   user_id: string;
-  role: string;
+  role: Role;
   iss: string;
   aud: string;
   iat?: number;
